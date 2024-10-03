c4d2e6f796a7680f12ef27bfa0ccdbbca77f8d70
Tvalimak <tanelivalimaki@gmail.com>
2024-10-03 20:52:28 +0300

    added ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvalimak <tvalimak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 00:54:21 by tvalimak          #+#    #+#             */
+/*   Updated: 2024/10/02 21:44:16 by tvalimak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main() {
+    Fixed a;
+    Fixed const b(Fixed(5.05f) * Fixed(2));
+    
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max(a, b) << std::endl;
+
+    return 0;
+}
